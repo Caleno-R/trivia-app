891249993ff5d6705c278316b4779acdd6eea86c
Caleno-R <calenorose@gmail.com>
2022-08-31 13:12:56 +0300

    fixed search endpoint

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -144,34 +144,48 @@ def create_app(test_config=None):
         search_term = body.get('search', None)
 
         try:
-            if search_term:
+            # Search for case insensitive strings
+            search_results = Question.query.filter(
+                Question.question.ilike('%{}%'.format(search_term))
+            )
+            questions_formatted = [question.format()
+                                   for question in search_results]
 
-                # Search for case insensitive strings
-                search_results = Question.query.order_by(Question.id).filter(
-                    Question.title.ilike('%{}%'.format(search_term))
-                )
-                questions_formatted = [search_results.format()
-                                       for question in search_results]
-
-                return jsonify({
-                    'success': True,
-                    'questions': questions_formatted,
-                    'current_category': None,
-                    'total_questions': len(search_results.all())
-
-                })
+            return jsonify({
+                'success': True,
+                'search': questions_formatted,
+                # 'body': search_results,
+                'current_category': None,
+                'total_questions': len(search_results.all())
+            })
         except Exception as e:
             print(sys.exc_info())
             abort(422)
 
     """
-    @TODO:
-    Create a GET endpoint to get questions based on category.
+    @TODO: Create a GET endpoint to get questions based on category.
 
     TEST: In the "List" tab / main screen, clicking on one of the
     categories in the left column will cause only questions of that
     category to be shown.
     """
+
+    @app.route('/categories<int:category_id>/questions')
+    def question_by_category(category_id):
+        try:
+            questions = Question.query.filter(
+                Question.category == category_id.all())
+            questions_formatted = [question.format() for question in questions]
+
+            return jsonify({
+                'succeed': True,
+                'questions': questions_formatted,
+                'current_categories': category_id,
+                'total_questions': len(questions),
+            })
+        except Exception:
+            print(sys.exc_info())
+            abort(422)
 
     """
     @TODO:
@@ -184,31 +198,30 @@ def create_app(test_config=None):
     one question at a time is displayed, the user is allowed to answer
     and shown whether they were correct or not.
     """
-
-    """
-    @TODO:
-    Create error handlers for all expected errors
-    including 404 and 422.
-    """
+    @app.route('/questions', methods=['POST'])
+    def get_quiz_questions():
+        try:
+            # Get data from the request body
+            body = request.get_json()
+            print(body)
+        except Exception as e:
+            print(sys.exc_info())
+            abort(422)
 
     @app.errorhandler(400)
     def bad_request(error):
-        return jsonify({"success": False, "error": 400, "message": "bad request"}), 400    
-
+        return jsonify({"success": False, "error": 400, "message": "bad request"}), 400
 
     @app.errorhandler(404)
     def not_found(error):
         return jsonify({"success": False, "error": 404, "message": "resource not found"}), 404
-    
+
     @app.errorhandler(405)
     def not_found(error):
         return jsonify({"success": False, "error": 405, "message": "method not allowed"}), 405
-    
+
     @app.errorhandler(422)
     def unprocessable(error):
         return jsonify({"success": False, "error": 404, "message": "resource not found"}), 422
-
-    
-
 
     return app
