19f830279eda04f32766b61361aa4b222a1bd408
Caleno-R <calenorose@gmail.com>
2022-09-02 09:50:20 +0300

    added another error handler

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -214,11 +214,15 @@ def create_app(test_config=None):
         return jsonify({"success": False, "error": 404, "message": "resource not found"}), 404
 
     @app.errorhandler(405)
-    def not_found(error):
+    def method_not_allowed(error):
         return jsonify({"success": False, "error": 405, "message": "method not allowed"}), 405
 
     @app.errorhandler(422)
     def unprocessable(error):
         return jsonify({"success": False, "error": 404, "message": "resource not found"}), 422
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({"success": False, "error": 500, "message": "internal server error"}), 500
 
     return app
